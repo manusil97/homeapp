184db61891c8fcedeeea18cd999c06f89cf85012
Emanuele Silvestri <emanuelesilvestri@par-152.local>
2018-09-27 09:30:50 +0200

    Home (Emanuele)

---
+++ homeapp/AlbumViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AlbumViewController.swift
+//  homeapp
+//
+//  Created by Emanuele Silvestri on 26/09/18.
+//  Copyright © 2018 iOSFoundation. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    @IBOutlet var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- homeapp/ViewController.swift
+++ homeapp/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate,CLLocationManagerDelegate {
 
+    @IBOutlet var userButton: UIBarButtonItem!
+    var userPosition: CLLocationCoordinate2D!
+    var locationManager: CLLocationManager!
+    @IBOutlet weak var map : MKMapView! = MKMapView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        sideMenu()
+        
+        map.delegate = self
+        userPosition = CLLocationCoordinate2D(latitude: 51.5085300, longitude:-0.1257400)
+        let span = MKCoordinateSpanMake(180, 320)
+        let region = MKCoordinateRegionMake(userPosition, span)
+        map.setRegion(region, animated: true)
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func sideMenu(){
+        if revealViewController() != nil {
+            userButton.target = revealViewController()
+            userButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 275
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            }
+    }
 
 }
-
